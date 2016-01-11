b80f7207e2fffcc78c016d4c5ba071a6b7fb367a
raulgomezn <saskeuchijacol@gmail.com>
2016-01-11 11:29:02 -0500

    Ajustes en el splash con animacion

--- app/src/main/java/com/vericarte/catalog/SplashScreen.java
+++ app/src/main/java/com/vericarte/catalog/SplashScreen.java
@@ -3,29 +3,57 @@ package com.vericarte.catalog;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by raulgomez on 10/01/16.
  */
 public class SplashScreen extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-        Thread timerThread = new Thread(){
+        final TextView titulo = (TextView) findViewById(R.id.textViewCatalog);
+        final ImageView imagen = (ImageView) findViewById(R.id.imageViewCatalog);
+
+        CargarAnimacion(titulo, imagen);
+
+        CargarMain();
+
+    }
+
+    private void CargarMain() {
+        Runnable r1 = new Runnable() {
+            @Override
             public void run(){
-                try{
-                    sleep(3000);
-                }catch(InterruptedException e){
-                    e.printStackTrace();
-                }finally{
-                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
-                    startActivity(intent);
-                }
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
             }
         };
-        timerThread.start();
+
+        Handler h1 = new Handler();
+        h1.postDelayed(r1, 2000);
+    }
+
+    private void CargarAnimacion(final TextView titulo, final ImageView imagen) {
+        Runnable r = new Runnable() {
+            @Override
+            public void run(){
+                titulo.setVisibility(View.VISIBLE);
+                imagen.setVisibility(View.VISIBLE);
+                titulo.startAnimation(AnimationUtils.loadAnimation(SplashScreen.this, android.R.anim.slide_in_left));
+                imagen.startAnimation(AnimationUtils.loadAnimation(SplashScreen.this, android.R.anim.slide_in_left));
+            }
+        };
+
+        Handler h = new Handler();
+        h.postDelayed(r, 1000);
     }
 
     @Override
